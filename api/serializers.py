bf1921dc495b44a7d98fa1fd3f000c28ea325e9d
AnishBade <badeanish6045@gmail.com>
2023-05-13 10:00:45 +0545

    first serializer added

--- api/serializers.py
+++ api/serializers.py
@@ -3,4 +3,5 @@ from api.models import Company
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model:Company
+        model=Company
+        fields="__all__"
